92c4f89d616fb15aae6960d953999a7e0c27eada
Jessica Kitchen <jess@Jessicas-Mac-mini.local>
2021-12-17 14:04:06 -0500

    fleshed out order component

--- src/Components/Order.js
+++ src/Components/Order.js
@@ -1,10 +1,26 @@
 import React from "react";
-import PizzaForm from "./PizzaForm";
 
 export default function Order(props) {
     const { details } = props
+    if(!details) {
+        return <h3>Fetching your Order...Thanks again for Choosing us!</h3>
+    }
 
     return(
-
+        <div className='order container'>
+            <h2>Here's your Order: </h2>
+            <p>Name: {details.name}</p>
+            <p>Size: {details.size}</p>
+            {
+                !!details.topping && !!details.toppings.length &&
+                <div>
+                    Toppings:
+                    <ul>
+                    {details.toppings.map((like, idx) => <li key={idx}>{like}</li>)}    
+                    </ul> 
+                </div>       
+            }
+            <p>Special Instructions: {details.special}</p>
+        </div>
     )
 }
