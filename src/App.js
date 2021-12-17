67cbc6d7a57211419666097400aee6d812c2daad
Jessica Kitchen <jess@Jessicas-Mac-mini.local>
2021-12-17 10:59:14 -0500

    completed routing and links in app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link, Route, Switch } from 'react-router-dom';
+
+import HomePage from './Components/HomePage';
+import PizzaForm from './Components/PizzaForm';
+import Confirmation from './Components/Confirmation';
+
 
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <h1>Bloomin' Eats</h1>
+      <div className='nav-links'>
+        <Link to='/'>Home</Link>
+        <Link to='/help'>Help</Link>
+        </div>
+      <div className='header-link'>
+        <Link to='/pizza'>Pizza?</Link>
+      </div>
+
+      <Switch>
+        <Route path='/pizza/confirmation'>
+          <Confirmation />
+        </Route>
+        <Route path='/pizza'>
+          <PizzaForm />
+        </Route>
+        <Route path='/'>
+          <HomePage />
+        </Route>
+      </Switch>
     </>
   );
 };
